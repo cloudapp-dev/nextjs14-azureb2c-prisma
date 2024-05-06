1903807a636ae8d8ceb27ba5b8515898014668cd
Lireking <avantsystem@gmail.com>
2024-05-06 12:00:55 +0200

    Changed RevalidatePath from GET to POST

--- src/app/api/revalidatepath/route.ts
+++ src/app/api/revalidatepath/route.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export async function GET(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get("secret");
-  const searchParams = new URL(request.url).searchParams;
-  const url = searchParams.get("url");
-
-  console.log("url", url);
+export async function POST(request: NextRequest) {
+  const secret = request.headers.get("secret");
+  const document = await request.json();
+  const url_en = "/en-US/" + document.slug;
+  const url_de = "/de-DE/" + document.slug;
 
   if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
-  revalidatePath(url || "/");
+  revalidatePath(url_en || "/");
+  revalidatePath(url_de || "/");
 
   // This will revalidate any URL that matches the provided page file on the next page visit.
   // This will not invalidate pages beneath the specific page.
@@ -30,7 +30,7 @@ export async function GET(request: NextRequest) {
   // // or with route groups
   // revalidatePath("/(main)/post/[slug]", "layout");
 
-  return new Response(`Revalidating ${url}`, {
+  return new Response(`Revalidating ${url_en} and ${url_de}`, {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
@@ -38,4 +38,4 @@ export async function GET(request: NextRequest) {
   });
 }
 
-// then call your-website-url/api/revalidatepath?secret=xxxx&url=url/that/needs/to/be/invalidated.
+// then call your-website-url/api/revalidatepath
