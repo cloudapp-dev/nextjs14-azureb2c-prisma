4192f7ca829aa50a62e755b626f38c29c4dac10b
Lireking <avantsystem@gmail.com>
2024-06-09 17:19:32 +0200

    Middleware Tracking

---
+++ src/app/api/user/tracking/route.ts
@@ -0,0 +1,43 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+const apikey = process.env.API_KEY;
+
+export async function POST(req: Request) {
+  if (req.headers.get("x-api-key") !== apikey) {
+    return new NextResponse(
+      JSON.stringify({ status: "fail", message: "You are not authorized" }),
+      { status: 401 }
+    );
+  }
+
+  const {
+    country,
+    city,
+    region,
+    url,
+    ip,
+    mobile,
+    platform,
+    useragent,
+    referer,
+  } = await req.json();
+
+  let data = await prisma.tracking.create({
+    data: {
+      country: country,
+      city: city,
+      region: region,
+      url: url,
+      ip: ip,
+      mobile: mobile,
+      platform: platform,
+      useragent: useragent,
+      referer: referer,
+    },
+  });
+
+  return NextResponse.json({
+    data,
+  });
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,38 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { fallbackLng, locales } from "@/app/i18n/settings";
 
+const apikey = process.env.API_KEY;
+
+async function sendToPrisma(message: any) {
+  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/tracking`, {
+    method: "POST",
+    body: JSON.stringify(message),
+    headers: new Headers({
+      "Content-Type": "application/json" || "",
+      "x-api-key": apikey || "",
+    }),
+  });
+}
+
 export function middleware(request: NextRequest) {
+  //Middleware to track user data
+  const { ip } = request;
+
+  const message = {
+    country: request.geo?.country,
+    city: request.geo?.city,
+    region: request.geo?.region,
+    url: request.url,
+    ip: ip,
+    mobile: request.headers.get("sec-ch-ua-mobile"),
+    platform: request.headers.get("sec-ch-ua-platform"),
+    useragent: request.headers.get("user-agent"),
+    referer: request.headers.get("referer"),
+  };
+  // console.log("message", message);
+
+  sendToPrisma(message);
+
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
 
